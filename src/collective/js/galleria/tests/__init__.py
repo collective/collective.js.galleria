035996c30ce432cc283c94900f912de27943b4ac
wesleybl <wesleybl@gmail.com>
2022-05-05 14:52:46 -0300

    Load zcml of collective.js.galleria.tests in buildout

--- src/collective/js/galleria/tests/__init__.py
+++ src/collective/js/galleria/tests/__init__.py
@@ -1,1 +1,10 @@
 # -*- coding: utf-8 -*-
+from Products.CMFPlone.interfaces import INonInstallable
+from zope.interface import implementer
+
+
+@implementer(INonInstallable)
+class HiddenProfiles(object):
+    def getNonInstallableProducts(self):
+        """Hide tests profiles from prefs_install_products_form."""
+        return ["collective.js.galleria.tests"]

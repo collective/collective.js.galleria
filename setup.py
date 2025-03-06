1fab3ebc77e079b3f24a2b7dd1dd902a7544115b
Maurício Victor <mauvictor@gmail.com>
2025-03-06 16:28:00 -0300

    Fix invalid specifier erro in python_requires.
    error in collective.js.galleria setup command: 'python_requires' must be a string containing valid version specifiers; Invalid specifier: '!=3.4.*!=3.5.*'

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*!=3.5.*",
+    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*",
     install_requires=[
         "setuptools",
         "plone.app.registry",

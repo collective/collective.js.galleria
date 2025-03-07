4bd40aa57b480304bf690f66de4f4f25ccfe9b02
wesleybl <wesleybl@gmail.com>
2025-03-07 15:59:20 -0300

    Upgrade code to Python 3

--- src/collective/js/galleria/setuphandlers.py
+++ src/collective/js/galleria/setuphandlers.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 from Products.CMFPlone.interfaces import INonInstallable
 from zope.interface import implementer
 
 
 @implementer(INonInstallable)
-class HiddenProfiles(object):
+class HiddenProfiles:
     def getNonInstallableProducts(self):
         """Hide the upgrades package from prefs_install_products_form."""
         return ["collective.js.galleria.upgrades"]

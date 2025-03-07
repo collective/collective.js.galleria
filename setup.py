cf539acdf4d1929fa386c3099cf1c04bec496cf1
Maurício Victor <mauvictor@gmail.com>
2025-03-07 12:05:53 -0300

    ython_requires can be modified to accept Python >= 3.7
    Even though the product still works on Python < 3.7, since it is no longer tested on those versions and they are old versions, python_requires can be modified to accept Python >= 3.7

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
     package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*",
+    python_requires=">=3.7",
     install_requires=[
         "setuptools",
         "plone.app.registry",
